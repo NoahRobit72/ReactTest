f319eab9a4b22a5dbe243c1c6e6db70d7e159034
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-04-26 12:19:59 -0400

    add comments

--- research-app/src/components/LabInfo.js
+++ research-app/src/components/LabInfo.js
@@ -4,10 +4,12 @@ function LabInfo({ lab }) {
   return (
     <div>
       <hr></hr>
+      <div className= "contents">
       <p>Lab Name: {lab.name}</p>
       <p>Rating: {lab.score}</p>
       <p>Professor: {lab.professor}</p>
       <hr></hr>
+      </div>
     </div>
   );
 }
--- research-app/src/pages/LabsBU.js
+++ research-app/src/pages/LabsBU.js
@@ -2,13 +2,24 @@ import React from "react"
 import HeaderSchool from "../components/HeaderSchool";
 import LabInfo from "../components/LabInfo"
 
+// EXAMPLE DATA
 const labs = [
     { name: 'Lab A', score: 80, professor: 'Dr. Smith' },
     { name: 'Lab B', score: 90, professor: 'Dr. Johnson' },
     { name: 'Lab C', score: 85, professor: 'Dr. Lee' },
   ];
 
-  
+//
+// KFIR CODE
+//
+
+// Make a script to query the firebase data base for all the labs at BU
+
+// ie make a function "GetLabs(BU)" that returns:
+// { name: 'Lab A', score: 80, professor: 'Dr. Smith' },
+// { name: 'Lab B', score: 90, professor: 'Dr. Johnson' },
+// { name: 'Lab C', score: 85, professor: 'Dr. Lee' },
+
 function LabsBU() {
     return (
         <div className="labsBU">
