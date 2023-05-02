0b7e1501baea4657c2f39938fadbdc0661e240b8
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2023-05-01 20:45:59 -0400

    add file'

--- research-app/src/pages/LoginPages/LoginPage.js
+++ research-app/src/pages/LoginPages/LoginPage.js
@@ -1,26 +1,63 @@
 import React from "react"
 import "../../css/LoginPage.css"
+import { useState } from 'react';
 
 
-export function LabsBU() {
-    const [formData, setFormData] = React.useState({
-        firstName: "", lastName: "", email: "", password: "", password2: ""
+// import { initializeApp } from "firebase/app";
+// import { getDatabase, ref, set } from "firebase/database";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDnvTbZTBGEP0D40K0ohmqDQsFVP3Iy-lU",
+//     authDomain: "ratingsreview-f04f9.firebaseapp.com",
+//     projectId: "ratingsreview-f04f9",
+//     storageBucket: "ratingsreview-f04f9.appspot.com",
+//     messagingSenderId: "984944313406",
+//     appId: "1:984944313406:web:52a529d0d3b00c3e18bd6f",
+//     measurementId: "G-0QPK4X29BL"
+//   };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+// const auth = getAuth();
+
+
+
+export function LoginPage() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        firstName: "",
+        password: "",
+        password2: "",
     })
 
+    const [showError, setShowError] = useState(false); // initialize state for error message
+
+    
     // Parse the inputs and same them to variables 
     function handleChange(event) {
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [event.target.name]: event.target.value
-            }
-        })
+        const{name,value} = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]:value
+        }))
     }
 
-    // what actions to take when we submit
+    // On submit send this value to the firebase login 
     function handleSubmit(event) {
-        event.preventDefault();// dont refresh the page
-        console.log(formData)
+        event.preventDefault()
+        if(formData.password === formData.password2){
+            console.log("user submitted")
+            setShowError(false);
+
+        }
+        else{
+            console.log("user not submitted")   
+            setShowError(true);
+        }
     }
 
     return (
@@ -32,13 +69,6 @@ export function LabsBU() {
                 onChange = {handleChange}
                 name="firstName"
                 value={formData.firstName}
-            />
-            <input
-                type="text"
-                placeholder="LastName"
-                onChange = {handleChange}
-                name="lastName"
-                value={formData.lastName}
             />
             <input
                 type="text"
@@ -61,7 +91,9 @@ export function LabsBU() {
                 name="password2"
                 value={formData.password2}
             />
+            {showError && <p>Passwords do not match. Please try again.</p>}
             <button>Submit</button>
+            
         </form>
     </div>
     )
