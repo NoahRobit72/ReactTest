e99d7356be5e295a8ca51ca33bc6bfd5dbcb2dee
kflank <kfirflank@gmail.com>
2023-07-24 23:12:13 -0400

    review submission without working firebase storing

--- research-app/src/pages/SubmissionPages/BlankPage.js
+++ research-app/src/pages/SubmissionPages/BlankPage.js
@@ -1,14 +1,121 @@
-import React from "react";
-import { useLocation } from "react-router-dom"; // Import useLocation to access the query parameters
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { TextField, Button, Typography, Container, Box } from "@mui/material";
+import { doc, collection, addDoc } from "firebase/firestore";
+import { FSDB } from "../../firebase_setup/firebase";
+import "../../css/BlankPage.css";
+
+
 
 function BlankPage() {
-  const location = useLocation(); // Use useLocation hook to access the query parameters
+  const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const selectedOption = searchParams.get("selectedOption");
+  const labId = searchParams.get("labId");
+
+  const [labName, setLabName] = useState("");
+  const [labField, setlabField] = useState("");
+  const [labProfessor, setlabProfessor] = useState("");
+  const [position, setPosition] = useState("");
+  const [rating, setRating] = useState("");
+  const [review, setReview] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create a new review document in the Reviews sub-collection under the Lab document
+    const reviewData = {
+      Position: position,
+      Rating: rating,
+      Review: review,
+    };
+
+    try {
+      // Add the review data to the Reviews sub-collection
+      await addDoc(collection(FSDB, "Universities", selectedOption, "Labs", labId, "Reviews"), reviewData);
+
+      // Reset form fields after successful submission
+      setLabName("");
+      setlabField("");
+      setlabProfessor("");
+      setPosition("");
+      setRating("");
+      setReview("");
+
+      // Navigate back to the LabsPage after submission
+      navigate(`/labs/${selectedOption}`);
+    } catch (error) {
+      console.error("Error adding review: ", error);
+    }
+  };
 
   return (
     <div className="signup-container">
-      <h1>This is a signup page {selectedOption}</h1>
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4, display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <Typography variant="h4" gutterBottom>
+            Review Submission for {selectedOption}
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 2 }}>
+            <TextField
+              required
+              fullWidth
+              label="Lab Name"
+              value={labName}
+              onChange={(e) => setLabName(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Field"
+              value={labField}
+              onChange={(e) => setlabField(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Professor"
+              value={labProfessor}
+              onChange={(e) => setlabProfessor(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Position"
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Rating"
+              type="number"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              required
+              fullWidth
+              label="Review"
+              multiline
+              rows={4}
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              margin="normal"
+            />
+            <Button type="submit" variant="contained" color="primary" sx={{ mt: 2 }}>
+              Submit Review
+            </Button>
+          </Box>
+        </Box>
+      </Container>
     </div>
   );
 }
