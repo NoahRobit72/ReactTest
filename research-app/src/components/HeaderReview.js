967fe8cfa8ac6d133ea85f07d3bd3ef3ebe52b6d
kflank <kfirflank@gmail.com>
2023-07-25 23:44:49 -0400

    add first review rendering if no reviews are present in the collection

--- research-app/src/components/HeaderReview.js
+++ research-app/src/components/HeaderReview.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import plusLogo from "../static/plus.png";
 import '../css/Header.css';
+import { collection, getDocs } from "firebase/firestore";
+import { FSDB } from "../firebase_setup/firebase";
 
 function HeaderReview({ labId, collegeName }) {
+  const [reviewsCount, setReviewsCount] = useState(0);
+
+  useEffect(() => {
+    const fetchReviewsCount = async () => {
+      try {
+        // Fetch reviews collection for the specific lab
+        const labRef = collection(FSDB, "Universities", collegeName, "Labs", labId, "Reviews");
+        const querySnapshot = await getDocs(labRef);
+
+        // Set the reviews count
+        setReviewsCount(querySnapshot.size);
+      } catch (error) {
+        console.error("Error fetching reviews: ", error);
+      }
+    };
+
+    fetchReviewsCount();
+  }, [collegeName, labId]);
+
   return (
     <header>
       <Link className="site-logo" to="/">ResearchReviews.com</Link>
@@ -13,11 +34,12 @@ function HeaderReview({ labId, collegeName }) {
           <button className="addLab">
             <div className="buttonBox">
               <img src={plusLogo} alt="BigCo Inc. logo" />
-              <p className="request">Add a Review </p>
+              <p className="request">
+                {reviewsCount === 0 ? "Add the first Review!" : "Add a Review"}
+              </p>
             </div>
           </button>
         </Link>
-        
       </nav>
     </header>
   );
